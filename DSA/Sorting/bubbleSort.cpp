ed05a7429c0ea4a96598ea3425c92571e90c57f8
Jatin <jatinpathania1@gmail.com>
2025-08-24 17:49:23 +0530

    optimised bubbleSort

--- DSA/Sorting/bubbleSort.cpp
+++ DSA/Sorting/bubbleSort.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 void bubbleSort(int arr[],int n){
     for(int i=0 ;i < n-1 ; i++){
+        bool swapped= false;
         for(int j= 0 ; j< n-i-1 ; j++){
             if(arr[j]> arr[j+1]){
                 //swap
                 int temp= arr[j];
                 arr[j]= arr[j+1];
                 arr[j+1]= temp;
+                swapped= true;
             }
         }
+        if(swapped== false) break;
     }
 }
 
