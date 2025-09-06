e79b4bb2381853678706750ef7053a336ec53103
Jatin <jatinpathania1@gmail.com>
2025-09-06 18:11:00 +0530

    linear search

--- DSA/Arrays/linearSearch.cpp
+++ DSA/Arrays/linearSearch.cpp
@@ -1,18 +1,16 @@
 #include<stdio.h>
 using namespace std;
 
-int search(int arr[],int n,int num)
-{
+int search(int arr[],int n,int num){
     int i;
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]==num)
+    for(i=0; i<n; i++){
+        if(arr[i] == num)
         return i;
     }
     return -1;
 }
-int main()
-{
+
+int main(){
     int arr[]={1,2,3,4,5};
     int num = 4;
     int n = sizeof(arr)/sizeof(arr[0]);
