330cddaf738c720ccb3c3bb454251ec20ce734ff
Jatin <jatinpathania1@gmail.com>
2025-09-16 14:56:23 +0530

    Two sum problem

---
+++ DSA/Arrays/twoSum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Optimal solution
+vector<int> twoSum(int n, vector<int> &arr, int target){
+    unordered_map<int, int> mpp;
+    for(int i=0; i<n; i++){
+        int num= arr[i];
+        int more= target- arr[i];
+        if(mpp.find(more)!= mpp.end()){
+            return {mpp[more], i};
+        }
+        mpp[num]= i;
+    }
+    return {-1,-1};
+}
+
+int main(){
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    vector<int> ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 2: [" << ans[0] << ", "<< ans[1] << "]" << endl;
+    return 0;
+}
