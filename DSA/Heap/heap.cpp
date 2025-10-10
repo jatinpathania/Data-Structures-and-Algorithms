fd4e5027fba025672f94fba26d0d7c5727ecc6fa
Jatin <jatinpathania1@gmail.com>
2025-10-10 20:22:40 +0530

    heap

---
+++ DSA/Heap/heap.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMin(int arr[]){
+
+}
+
+int main(){
+    int heap[]= {3,5,6,8,10,9,7};
+    int res= getMin(heap);
+    cout<<res<<endl;
+}   
