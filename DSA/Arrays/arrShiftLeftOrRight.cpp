48a67578d1a621f5e044f181af20888b6143c391
Jatin <jatinpathania1@gmail.com>
2025-10-27 11:59:43 +0530

    array partition

---
+++ DSA/Arrays/arrShiftLeftOrRight.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// optimal sol
+void shiftLeftOrRight(vector<int> arr, int x){
+    int j=0;
+    for(int i=0; i< arr.size() ;i++){
+        if(arr[i] <=x){
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    for(int i=0; i<arr.size(); i++) cout<<arr[i]<<" ";
+}
+
+// better sol
+// void shiftLeftOrRight(vector<int> arr, int x){
+//     int n= arr.size();
+//     vector<int> ans(n);
+//     int left=0;
+//     int right= n-1;
+//     for(int i=0; i<n ;i++){
+//         if(arr[i] < x) ans[left++]= arr[i];
+//         else ans[right--] = arr[i];
+//     }
+
+//     for(int i=0; i<ans.size(); i++) cout<<ans[i]<<" ";
+// }
+
+int main(){
+    vector<int> arr= {7,2,9,3,1,8,4};
+    int x=6;
+    shiftLeftOrRight(arr, x);
+}
