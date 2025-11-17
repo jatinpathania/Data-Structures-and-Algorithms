004a79cccfd746016bf2b60aedc3f865b6bcd2b1
Jatin <jatinpathania1@gmail.com>
2025-11-17 12:03:44 +0530

    no of subarrays with xor k

---
+++ DSA/Arrays/noOfsubArrWithXorK.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//optimal approach- O()
+int subarraysWithXorK(vector<int> a, int k){
+    map<int,int> mpp;
+    int Xor=0;
+    int count=0;
+    mpp[0]= 1;            // or simply write mpp[Xor]++ to store {0,1} in map
+    for(int i=0; i<a.size(); i++){
+        Xor^= a[i];
+        int ele= Xor^k;
+        count+= mpp[ele];         // stores 0 if not found.. we didn't use find here because storing 0 in mpp is harmless in this case
+        mpp[Xor]++; 
+    }
+    return count;
+}
+
+int main(){
+    vector<int> a = {4, 2, 2, 6, 4};
+    int k = 6;
+    int ans = subarraysWithXorK(a, k);
+    cout << "The number of subarrays with XOR k is: " << ans << "\n";
+    return 0;
+}
