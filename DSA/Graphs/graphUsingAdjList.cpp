76dd30194484ddb98aea93670766e1d015f74463
Jatin <jatinpathania1@gmail.com>
2025-11-17 12:03:18 +0530

    graphs started

---
+++ DSA/Graphs/graphUsingAdjList.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph1{
+    private:
+    int n;
+    vector<vector<int>> adj;
+
+    public:
+    Graph1(int n){
+        this->n= n;
+        adj.resize(n);
+    }
+
+    void addEdge(int u,int v){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void print(){
+        for(int i=0; i<n; i++ ){
+            cout<<i<< " -> ";
+            for(int node: adj[i]){
+                cout<< node <<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    int n=4;
+    Graph1 graph(n);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 0);
+
+    graph.print();
+}
---
+++ DSA/Graphs/graphUsingAdjMatrix.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Graph1{
+    private:
+    int n;
+    vector<vector<int>> adjacencyMatrix;
+
+    public:
+    Graph1(int n){
+        this->n= n;
+        adjacencyMatrix= vector<vector<int>>(n, vector<int>(n,0));
+    }
+
+    void addEdge(int u,int v){
+        adjacencyMatrix[u][v]= 1;
+        adjacencyMatrix[v][u]= 1;
+    }
+
+    void print(){
+        for(int i=0; i<n ;i++){
+            for(int j=0; j< n; j++){
+                cout<< adjacencyMatrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    int n=4;
+    Graph1 graph(n);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 0);
+
+    graph.print();
+}
