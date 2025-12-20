2667b7307925888c28669a3de78f324df374b136
Jatin <jatinpathania1@gmail.com>
2025-12-20 12:45:58 +0530

    jump game

---
+++ DSA/Greedy Algorithms/jumpGame.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Optimal- O(N)
+class JumpGame {
+public:
+    bool canJump(vector<int>& nums){
+        int maxIndex=0;
+        for(int i=0; i<nums.size()-1 ;i++){
+            if(i > maxIndex) return false;
+            maxIndex= max(maxIndex, i+ nums[i]);
+        }
+        return true;
+    }
+};
+
+int main() {
+    vector<int> nums = {4, 3, 7, 1, 2};
+    cout << "Array representing maximum jump from each index: ";
+    for (int val : nums) cout << val << " ";
+    cout << endl;
+    JumpGame game;
+    bool ans = game.canJump(nums);
+    if (ans)
+        cout << "It is possible to reach the last index." << endl;
+    else
+        cout << "It is not possible to reach the last index." << endl;
+    return 0;
+}
