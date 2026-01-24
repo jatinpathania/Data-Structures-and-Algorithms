a6e8d22535b805c99782f070c8a5727eac1bd276
Jatin <jatinpathania1@gmail.com>
2026-01-24 12:17:16 +0530

    upperbound

---
+++ DSA/Binary_Search/upperBound.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Time Complexity: O(logn)
+// Space Complexity: O(1) 
+// find the smallest element greater than given number
+class UpperBoundFinder {
+public:
+    int upperBound(vector<int> arr, int n, int x){
+        int low=0, high= n-1;
+        int ans= n;
+        while(low <= high){
+            int mid= low+ (high-low)/2;
+            if(arr[mid] > x){
+                ans= mid;
+                high= mid-1;
+            }
+            else{
+                low= mid+1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = arr.size();
+    int x = 9;
+    UpperBoundFinder finder;
+    int ind = finder.upperBound(arr, n, x);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
+//Shortcut for cpp-  upper_bound(arr.begin(), arr.end(), x)- arr.begin();
